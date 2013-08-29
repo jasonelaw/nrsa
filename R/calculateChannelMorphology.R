d7e05e7643c490b7b85699daed59c00142e430b7
Jason Law <jason.e.law@gmail.com>
2013-08-29 14:38:13 -0700

    Finish refactor of channel morphology; replicates EPA test data results.

--- R/calculateChannelMorphology.R
+++ R/calculateChannelMorphology.R
@@ -16,6 +16,9 @@ sumNA <- function(x, ...){
 #' @importFrom plyr ddply
 #' @importFrom stringr str_replace
 #' @export
+#' @examples
+#' d <- data.frame(uid = c(1,1), transect = c('A', 'XA'), station = c(0,0), wetwid = 1:2, bankwid = 1:2, bankhgt = 1:2, incishgt = 1:2)
+#' do.call('joinExtraTransects', d)
 joinExtraTransects <- function(uid, transect, station, wetwid, bankwid, bankhgt, incishgt){
   transect <- str_replace(transect, 'X', '')
   x <- data.frame(uid, transect, station, wetwid, bankwid, bankhgt, incishgt)
@@ -23,8 +26,8 @@ joinExtraTransects <- function(uid, transect, station, wetwid, bankwid, bankhgt,
     suppressWarnings(
       c(wetwid.sm   = sumNA(x$wetwid, na.rm = T),
         wetwid.mx   = max(x$wetwid, na.rm = T),
-        bankwid     = sumNA(x$bankwid, na.rm = T),
-        incishgt    = max(x$incishgt, na.rm = T),
+        bankwid.sm  = sumNA(x$bankwid, na.rm = T),
+        incishgt.mx = max(x$incishgt, na.rm = T),
         bankhgt.mx  = max(x$bankhgt, na.rm = T),
         bankhgt.mn  = mean(x$bankhgt, na.rm = T)))
   }
@@ -42,12 +45,17 @@ joinExtraTransects <- function(uid, transect, station, wetwid, bankwid, bankhgt,
 
 #' Calculate channel metrics
 #' 
-#' Calculate channel metrics
-#' @param uid
-#' @param wetwid
-#' @param bankwid
-#' @param incishgt
-#' @param bankhgt
+#' Calculate the channel metrics: xbkf_w, sdbkf_w, n_bw, xinc_h, sdinc_h, n_incis, 
+#' xbkf_h, sdbkf_h, n_bh.
+#' 
+#' The EPA uses data where the extra transect - transect pairs have been joined.  
+#' The \code{bankwid} argument is the sum of the extra transect pairs, the \code{incishgt} 
+#'  and \code{bankhgt} arguments are the max of the extra transect pairs.  These can 
+#'  be calculated using \link{joinExtraTransects}.
+#' @param uid a vector of site identifiers
+#' @param bankwid a vector of bankfull width measurements; sum of extra transect pairs
+#' @param incishgt a vector of incision heights; max of extra transect pairs
+#' @param bankhgt a vector of bankfull heights; max of extra transect pairs
 #' @export
 #' @examples
 #' d <- data.frame(uid = rep(1:10, 11), wetwid = runif(110), bankwid = runif(110), incishgt = runif(110), bankhgt = runif(110))
@@ -74,13 +82,12 @@ calculateChannelMetrics <- function(uid, bankwid, incishgt, bankhgt){
 
 #' Calculates wetted width metrics
 #' 
-#' Calculates wetted width metrics
+#' Calculates wetted width metrics: xwidth, sdwidth, and n_w.
 #' 
 #' Uses the max wetted width of the transect - extra transect pair as returned 
 #' by \link{joinExtraTransects}.
 #' @param uid a vector of site identifiers
-#' @param wetwid a vector of wetted widths; uses the max of the transect - extra
-#'  transect pair
+#' @param wetwid a vector of wetted widths; uses the max of the extra transect pairs
 #' @return a 'metric' data.frame
 #' @importFrom plyr ddply
 #' @importFrom NARSShared count
@@ -100,8 +107,11 @@ calculateWettedWidthMetrics <- function(uid, wetwid){
 #' Calculate thalweg width depth ratios
 #' 
 #' Calculate metrics dealing with the product and ratio of the channel width and
-#' depth
-#' @param uid
+#' depth: xwxd, sdwxd, n_wd, xwd_rat, sdwd_rat, and n_wdr.
+#' 
+#' These are station level data (i.e., from the thalweg form) and so do not need any special
+#' extra transect treatment.
+#' @param uid a vector site identifiers
 #' @param wetwid
 #' @param depth
 #' @return a 'metric' data.frame
@@ -123,9 +133,24 @@ calculateThalwegRatios <- function(uid, wetwid, depth){
   meltMetrics(ans)
 }
 
+#' Calculates metrics of bankfull width to (bankfull height + depth) ratios
+#' 
+#' This function calculates summary metrics of the ratio: bankfull width / 
+#' (bankfull height + thalweg depth).  The mean and count of measurements are
+#' calculated.  
+#' 
+#' The EPA calculations used extra trnasects in these calculations: 
+#' sum of the bankfull widths at the extra transect and regular transect and the
+#' mean of the bankfull height at the extra transect and the regular transect.  
+#' These values are returned by the \link{joinExtraTransects} function.
+#' @param uid a vector of site identifiers
+#' @param bankwid a vector of bankfull widths; EPA uses the sum of the extra transects.
+#' @param bankhgt a vector of bankfull heights; EPA uses the mean of the extra transects.
+#' @param depth a vector of thalweg depths
+#' @importFrom plyr ddply
 #'@export
 calculateTransectRatios <- function(uid, bankwid, bankhgt, depth){
-  bfwd <- bankwid / (bankght + depth)
+  bfwd <- bankwid / (bankhgt + depth)
   x <- data.frame(uid, bfwd)
   f <- function(x){
     c('bfwd_rat' = mean(x$bfwd, na.rm = T),
@@ -135,14 +160,36 @@ calculateTransectRatios <- function(uid, bankwid, bankhgt, depth){
   meltMetrics(ans)
 }
 
-#'@export
-calculateThalwegDepthMetrics <- function(uid, depth){
+#' Calculate thalweg depth metrics: xdepth, sddepth, n_d
+#' 
+#' Calculates the thalweg depth metrics (xdepth, sddepth, n_d) form thalweg depth
+#' data.
+#' 
+#' The EPA code assumes input data in meters, which this function also assumes.
+#' However, the EPA code reports the metrics for wadeable sites in centimeters.
+#' This code includes an argument \code{units}, which allows the user to specify
+#' the desired output units.  The \code{mixed} option mimics the EPA behavior.
+#' @param uid a vector of site identifiers
+#' @param is.wadeable a logical vector, TRUE for each depth that is from
+#' a wadeable protocol site (versus a non-wadeable protocol).
+#' @param depth a vector of thalweg depths
+#' @param desired output units.  Can be one of \code{"m"}, \code{"cm"}, or \code{"mixed"}.
+#' @return a 'metric' data.frame
+#' @importFrom plyr ddply
+#' @importFrom NARSShared count
+#' @export
+calculateThalwegDepthMetrics <- function(uid, is.wadeable, depth, units = c('m', 'cm', 'mixed')){
   #ensure units match: some notes and code about converting from m to cm
+  units <- match.arg(units)
+  depth <- switch(units,
+                  m = depth,
+                  cm = depth * 100,
+                  mixed = ifelse(is.wadeable, depth * 100, depth))
   x <- data.frame(uid, depth)
   f <- function(x){
     c(xdepth  = mean(x$depth, na.rm = T),
       sddepth = sd(x$depth, na.rm = T),
-      n_d     = count(x$depth, na.rm = T))
+      n_d     = count(x$depth))
   }
   ans <- ddply(x, .(uid), f)
   meltMetrics(ans)
