6b6ac980cf7dba4bf26e39ef8561b1c639b3ad62
Jason Law <jason.e.law@gmail.com>
2013-10-07 13:41:44 -0700

    remove unnecessary code including all data validation code.

--- R/mergeMetrics.R
+++
@@ -1,7 +0,0 @@
-mergeMetrics <- function(...){
-  metrics <- list(...)
-  metrics <- rbind.fill(metrics)
-  metrics <- melt(metrics, id.var = 'uid', na.rm = T)
-  metrics <- dcast(metrics, uid ~ variable)
-  return(metrics)
-}
